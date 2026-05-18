8eb95781040c4714aa40505e6795255349435dfc
sarakey <sarrahquijano@gmail.com>
2026-05-18 22:29:16 +0800

    feat: add navbar partial

---
+++ app/views/partials/navbar.php
@@ -0,0 +1,18 @@
+<header class="bg-blue-900 text-white p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-3xl font-semibold">
+            <a href="/">JobSeeker</a>
+        </h1>
+        <nav class="space-x-4">
+            <?php if (!isset($_SESSION['user_id'])): ?>
+                <a href="/auth/login" class="text-white hover:underline">Login</a>
+                <a href="/auth/register" class="text-white hover:underline">Register</a>
+            <?php else: ?>
+                <a href="/auth/login" class="text-white hover:underline">Logout</a>
+            <?php endif; ?>
+            <a href="/listings/create"
+                class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i
+                    class="fa fa-edit"></i> Post a Job</a>
+        </nav>
+    </div>
+</header>
