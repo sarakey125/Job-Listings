887d48681e645eea01a32aa2d279f88fa60971b4
sarakey <sarrahquijano@gmail.com>
2026-05-19 20:41:55 +0800

    feat: update footer and navbar for improved layout and navigation

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,3 +1,7 @@
+<footer class="bg-white py-4 text-center text-sm text-gray-600 site-footer">
+    <p>Made by Sarrah Quijano</p>
+</footer>
+
 </body>
 
 </html>
--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -3,12 +3,14 @@
         <h1 class="text-3xl font-semibold">
             <a href="/">JobSeeker</a>
         </h1>
-        <nav class="space-x-4">
+        <nav class="space-x-4 flex items-center">
             <?php if (!isset($_SESSION['user_id'])): ?>
                 <a href="/auth/login" class="text-white hover:underline">Login</a>
                 <a href="/auth/register" class="text-white hover:underline">Register</a>
             <?php else: ?>
                 <a href="/auth/login" class="text-white hover:underline">Logout</a>
+                <a href="/blog" class="text-white hover:underline">Blog</a>
+                <a href="/ternary" class="text-white hover:underline">Ternary</a>
             <?php endif; ?>
             <a href="/listings/create"
                 class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i
